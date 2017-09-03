6f626de841deaf30267da4359a886b8cc233ad2d
Sasha <alexplevako@gmail.com>
2017-09-03 10:26:33 +0000

    changed address to the scorum gitlab

--- ciscripts/buildsuccess.sh
+++ ciscripts/buildsuccess.sh
@@ -1,10 +1,5 @@
 #/bin/bash
-curl --silent -XPOST -H "Authorization: token $GITHUB_SECRET" https://api.github.com/repos/steemit/steem/statuses/$(git rev-parse HEAD) -d "{
-  \"state\": \"success\",
-  \"target_url\": \"${BUILD_URL}\",
-  \"description\": \"Jenkins-CI reports build succeeded!!\",
-  \"context\": \"jenkins-ci-steemit\"
-}"
+curl --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/6/statuses/$(git rev-parse HEAD)?state=success
 rm -rf $WORKSPACE/*
 # make docker cleanup after itself and delete all exited containers
 sudo docker rm -v $(docker ps -a -q -f status=exited) || true
