44ade8a21f97f4129ec27e8a5d206dfd9a854c47
Sasha <alexplevako@gmail.com>
2017-09-03 10:47:34 +0000

    debug buildpending.sh

--- ciscripts/buildpending.sh
+++ ciscripts/buildpending.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo "TOKEN: $GITLAB_TOKEN"
+env
 curl --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/6/statuses/$(git rev-parse HEAD)?state=pending
