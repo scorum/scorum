7ff93b46a35b2f6694541ec09a8f404c625feb5e
Alexander Plevako <alexplevako@gmail.com>
2017-09-11 16:38:22 +0300

    BLOC-11 -- Changed gitlab project id for CI

--- ciscripts/buildpending.sh
+++ ciscripts/buildpending.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/6/statuses/$(git rev-parse HEAD)?state=pending
+curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/3/statuses/$(git rev-parse HEAD)?state=pending
--- ciscripts/buildsuccess.sh
+++ ciscripts/buildsuccess.sh
@@ -1,5 +1,5 @@
 #/bin/bash
-curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/6/statuses/$(git rev-parse HEAD)?state=success
+curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/3/statuses/$(git rev-parse HEAD)?state=success
 rm -rf $WORKSPACE/*
 # make docker cleanup after itself and delete all exited containers
 sudo docker rm -v $(docker ps -a -q -f status=exited) || true
