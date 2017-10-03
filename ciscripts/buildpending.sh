b71387fa1df5134dcaa33a4709948c1b4f1ed1c6
Alexander Trigubov <alexander.trigubov@scorum.com>
2017-10-03 17:56:22 +0300

    DVPS-19. CI Optimization

--- ciscripts/buildpending.sh
+++ ciscripts/buildpending.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://gitlab.scorum.me/api/v4/projects/3/statuses/$(git rev-parse HEAD)?state=pending
+curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://$GITLAB_DOMAIN/api/v4/projects/$GITLAB_PROJECT_ID/statuses/$(git rev-parse HEAD)?state=pending
