26b0b7a9f35308bc44524a233da1efbf860d68ff
Alex Shkor <alex.shkor@gmail.com>
2017-10-16 11:32:54 +0300

    remove pow from witnes schedule

--- libraries/chain/witness_schedule.cpp
+++ libraries/chain/witness_schedule.cpp
@@ -104,46 +104,12 @@ void update_witness_schedule(database& db)
 
         auto num_elected = active_witnesses.size();
 
-        /// Add miners from the top of the mining queue
-        flat_set< witness_id_type > selected_miners;
-        selected_miners.reserve( wso.max_miner_witnesses );
-        const auto& gprops = db.get_dynamic_global_properties();
-        const auto& pow_idx      = db.get_index<witness_index>().indices().get<by_pow>();
-        auto mitr = pow_idx.upper_bound(0);
-        while( mitr != pow_idx.end() && selected_miners.size() < wso.max_miner_witnesses )
-        {
-            // Only consider a miner who is not a top voted witness
-            if( selected_voted.find(mitr->id) == selected_voted.end() )
-            {
-                // Only consider a miner who has a valid block signing key
-                if( db.get_witness( mitr->owner ).signing_key != public_key_type() )
-                {
-                    selected_miners.insert(mitr->id);
-                    active_witnesses.push_back(mitr->owner);
-                    db.modify( *mitr, [&]( witness_object& wo ) { wo.schedule = witness_object::miner; } );
-                }
-            }
-            // Remove processed miner from the queue
-            auto itr = mitr;
-            ++mitr;
-            db.modify( *itr, [&](witness_object& wit )
-            {
-                wit.pow_worker = 0;
-            } );
-            db.modify( gprops, [&]( dynamic_global_property_object& obj )
-            {
-                obj.num_pow_witnesses--;
-            } );
-        }
-
-        auto num_miners = selected_miners.size();
-
         /// Add the running witnesses in the lead
         fc::uint128 new_virtual_time = wso.current_virtual_time;
         const auto& schedule_idx = db.get_index<witness_index>().indices().get<by_schedule_time>();
         auto sitr = schedule_idx.begin();
         vector<decltype(sitr)> processed_witnesses;
-        for( auto witness_count = selected_voted.size() + selected_miners.size();
+        for( auto witness_count = selected_voted.size();
                 sitr != schedule_idx.end() && witness_count < SCORUM_MAX_WITNESSES;
                 ++sitr )
         {
@@ -153,8 +119,7 @@ void update_witness_schedule(database& db)
             if( sitr->signing_key == public_key_type() )
                 continue; /// skip witnesses without a valid block signing key
 
-            if( selected_miners.find(sitr->id) == selected_miners.end()
-                && selected_voted.find(sitr->id) == selected_voted.end() )
+            if(selected_voted.find(sitr->id) == selected_voted.end() )
             {
                 active_witnesses.push_back(sitr->owner);
                 db.modify( *sitr, [&]( witness_object& wo ) { wo.schedule = witness_object::timeshare; } );
@@ -162,7 +127,7 @@ void update_witness_schedule(database& db)
             }
         }
 
-        auto num_timeshare = active_witnesses.size() - num_miners - num_elected;
+        auto num_timeshare = active_witnesses.size() - num_elected;
 
         /// Update virtual schedule of processed witnesses
         bool reset_virtual_time = false;
@@ -261,7 +226,7 @@ void update_witness_schedule(database& db)
             }
         
 
-        assert( num_elected + num_miners + num_timeshare == active_witnesses.size() );
+        assert( num_elected + num_timeshare == active_witnesses.size() );
 
         db.modify( wso, [&]( witness_schedule_object& _wso )
         {
@@ -278,7 +243,6 @@ void update_witness_schedule(database& db)
             _wso.num_scheduled_witnesses = std::max< uint8_t >( active_witnesses.size(), 1 );
             _wso.witness_pay_normalization_factor =
                 _wso.top19_weight * num_elected
-                + _wso.miner_weight * num_miners
                 + _wso.timeshare_weight * num_timeshare;
 
             /// shuffle current shuffled witnesses
