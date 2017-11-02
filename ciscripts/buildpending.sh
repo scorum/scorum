f0d8989ba6d99e6ec569f16bae3c3a2117cf7864
Alexander Trigubov <alexander.trigubov@scorum.com>
2017-11-02 12:27:28 +0300

    Reconfig CI

--- ciscripts/buildpending.sh
+++ ciscripts/buildpending.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://$GITLAB_DOMAIN/api/v4/projects/$GITLAB_PROJECT_ID/statuses/$(git rev-parse HEAD)?state=pending
+# curl --silent --request POST -H "PRIVATE-TOKEN: $GITLAB_TOKEN" https://$GITLAB_DOMAIN/api/v4/projects/$GITLAB_PROJECT_ID/statuses/$(git rev-parse HEAD)?state=pending
