1dcc6c468dd3527f3cb9673811f09437e58abe57
Alexander Plevako <alexplevako@gmail.com>
2017-11-14 18:39:19 +0300

    bloc84 -- refactor escrow_transfer_operations tests

--- tests/tests/operation_tests.cpp
+++ tests/tests/operation_tests.cpp
@@ -2661,90 +2661,6 @@ BOOST_AUTO_TEST_CASE(escrow_transfer_authorities)
     FC_LOG_AND_RETHROW()
 }
 
-BOOST_AUTO_TEST_CASE(escrow_transfer_apply)
-{
-    try
-    {
-        BOOST_TEST_MESSAGE("Testing: escrow_transfer_apply");
-
-        ACTORS((alice)(bob)(sam))
-
-        fund("alice", 10000);
-        signed_transaction tx;
-
-        escrow_transfer_operation op;
-        op.from = "alice";
-        op.to = "bob";
-        op.scorum_amount = ASSET("1.000 TESTS");
-        op.escrow_id = 0;
-        op.agent = "sam";
-        op.fee = ASSET("0.100 TESTS");
-        op.json_meta = "";
-        op.ratification_deadline = db.head_block_time() + 100;
-        op.escrow_expiration = db.head_block_time() + 200;
-
-        BOOST_TEST_MESSAGE("--- falure when from cannot cover amount + fee");
-        op.scorum_amount.amount = 10000;
-        tx.operations.clear();
-        tx.signatures.clear();
-        tx.operations.push_back(op);
-        tx.sign(alice_private_key, db.get_chain_id());
-        SCORUM_REQUIRE_THROW(db.push_transaction(tx, 0), fc::exception);
-
-        BOOST_TEST_MESSAGE("--- failure when ratification deadline is in the past");
-        op.scorum_amount.amount = 1000;
-        op.ratification_deadline = db.head_block_time() - 200;
-        tx.operations.clear();
-        tx.signatures.clear();
-        tx.operations.push_back(op);
-        tx.sign(alice_private_key, db.get_chain_id());
-        SCORUM_REQUIRE_THROW(db.push_transaction(tx, 0), fc::exception);
-
-        BOOST_TEST_MESSAGE("--- failure when expiration is in the past");
-        op.escrow_expiration = db.head_block_time() - 100;
-        tx.operations.clear();
-        tx.signatures.clear();
-        tx.operations.push_back(op);
-        tx.sign(alice_private_key, db.get_chain_id());
-        SCORUM_REQUIRE_THROW(db.push_transaction(tx, 0), fc::exception);
-
-        BOOST_TEST_MESSAGE("--- success");
-        op.ratification_deadline = db.head_block_time() + 100;
-        op.escrow_expiration = db.head_block_time() + 200;
-        tx.expiration = db.head_block_time() + 100;
-        tx.operations.clear();
-        tx.signatures.clear();
-        tx.operations.push_back(op);
-        tx.sign(alice_private_key, db.get_chain_id());
-
-        auto alice_scorum_balance = alice.balance - op.scorum_amount - op.fee;
-        auto bob_scorum_balance = bob.balance;
-        auto sam_scorum_balance = sam.balance;
-
-        db.push_transaction(tx, 0);
-
-        const auto& escrow = db.get_escrow(op.from, op.escrow_id);
-
-        BOOST_REQUIRE(escrow.escrow_id == op.escrow_id);
-        BOOST_REQUIRE(escrow.from == op.from);
-        BOOST_REQUIRE(escrow.to == op.to);
-        BOOST_REQUIRE(escrow.agent == op.agent);
-        BOOST_REQUIRE(escrow.ratification_deadline == op.ratification_deadline);
-        BOOST_REQUIRE(escrow.escrow_expiration == op.escrow_expiration);
-        BOOST_REQUIRE(escrow.scorum_balance == op.scorum_amount);
-        BOOST_REQUIRE(escrow.pending_fee == op.fee);
-        BOOST_REQUIRE(!escrow.to_approved);
-        BOOST_REQUIRE(!escrow.agent_approved);
-        BOOST_REQUIRE(!escrow.disputed);
-        BOOST_REQUIRE(alice.balance == alice_scorum_balance);
-        BOOST_REQUIRE(bob.balance == bob_scorum_balance);
-        BOOST_REQUIRE(sam.balance == sam_scorum_balance);
-
-        validate_database();
-    }
-    FC_LOG_AND_RETHROW()
-}
-
 BOOST_AUTO_TEST_CASE(escrow_approve_validate)
 {
     try
@@ -4851,4 +4767,5 @@ BOOST_AUTO_TEST_CASE(comment_beneficiaries_apply)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
+
 #endif
