59b19e6ea3e33b8d0a74983feff828f4acf7cbd3
Andrew Masilevich <andrew.masilevich@scorum.com>
2017-12-08 19:02:05 +0300

    Add tests skeleton

---
+++ tests/tests/registration_check/pool_service.cpp
@@ -0,0 +1,22 @@
+#ifdef IS_TEST_NET
+#include <boost/test/unit_test.hpp>
+
+#include <scorum/chain/scorum_objects.hpp>
+#include <scorum/chain/dbs_registration_pool.hpp>
+
+#include "database_fixture.hpp"
+
+using namespace scorum;
+using namespace scorum::chain;
+using namespace scorum::protocol;
+
+BOOST_FIXTURE_TEST_SUITE(registration_pool_service_check, clean_database_fixture)
+
+SCORUM_TEST_CASE(any_check)
+{
+    //TODO
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+#endif
