af30e99969b2691b8900f5b98d7ca5f57218a8a9
Alexander Plevako <alexplevako@gmail.com>
2017-12-12 16:11:56 +0300

    create db_genesis class

--- libraries/chain/genesis.cpp
+++ libraries/chain/genesis.cpp
@@ -10,17 +10,17 @@
 
 #include <scorum/chain/pool/reward_pool.hpp>
 
+#include <scorum/chain/genesis.hpp>
+
 #include <fc/io/json.hpp>
 
-#define SCORUM_DEFAULT_INIT_PUBLIC_KEY "STM5omawYzkrPdcEEcFiwLdEu7a3znoJDSmerNgf96J2zaHZMTpWs"
+#define SCORUM_DEFAULT_INIT_PUBLIC_KEY "SCR5omawYzkrPdcEEcFiwLdEu7a3znoJDSmerNgf96J2zaHZMTpWs"
 #define SCORUM_DEFAULT_GENESIS_TIME fc::time_point_sec(1508331600);
 #define SCORUM_DEFAULT_INIT_SUPPLY (1000000u)
 
 namespace scorum {
 namespace chain {
 namespace utils {
-
-using namespace scorum::protocol;
 
 void generate_default_genesis_state(genesis_state_type& genesis)
 {
@@ -39,7 +39,6 @@ void generate_default_genesis_state(genesis_state_type& genesis)
 
 } // namespace utils
 
-//////////////////////////////////////////////////////////////////////////
 fc::time_point_sec database::get_genesis_time() const
 {
     return _const_genesis_time;
@@ -74,11 +73,13 @@ void database::init_genesis(const genesis_state_type& genesis_state)
         _const_genesis_time = genesis_state.initial_timestamp;
         create<chain_property_object>([&](chain_property_object& cp) { cp.chain_id = genesis_state.initial_chain_id; });
 
-        init_genesis_accounts(genesis_state);
-        init_genesis_witnesses(genesis_state);
-        init_genesis_witness_schedule(genesis_state);
-        init_genesis_global_property_object(genesis_state);
-        init_genesis_rewards(genesis_state);
+        db_genesis genesis(*this, genesis_state);
+
+        genesis.init_accounts();
+        genesis.init_witnesses();
+        genesis.init_witness_schedule();
+        genesis.init_global_property_object();
+        genesis.init_rewards();
 
         // Nothing to do
         for (int i = 0; i < 0x10000; i++)
@@ -90,15 +91,19 @@ void database::init_genesis(const genesis_state_type& genesis_state)
     FC_CAPTURE_AND_RETHROW()
 }
 
-void database::init_genesis_accounts(const genesis_state_type& genesis_state)
+scorum::chain::db_genesis::db_genesis(scorum::chain::database& db, const genesis_state_type& genesis_state)
+    : _db(db)
+    , _genesis_state(genesis_state)
 {
-    const vector<genesis_state_type::account_type>& accounts = genesis_state.accounts;
+}
 
-    for (auto& account : accounts)
+void db_genesis::init_accounts()
+{
+    for (auto& account : _genesis_state.accounts)
     {
         FC_ASSERT(!account.name.empty(), "Account 'name' should not be empty.");
 
-        create<account_object>([&](account_object& a) {
+        _db.create<account_object>([&](account_object& a) {
             a.name = account.name;
             a.memo_key = account.public_key;
             a.balance = asset(account.scr_amount, SCORUM_SYMBOL);
@@ -106,7 +111,7 @@ void database::init_genesis_accounts(const genesis_state_type& genesis_state)
             a.recovery_account = account.recovery_account;
         });
 
-        create<account_authority_object>([&](account_authority_object& auth) {
+        _db.create<account_authority_object>([&](account_authority_object& auth) {
             auth.account = account.name;
             auth.owner.add_authority(account.public_key, 1);
             auth.owner.weight_threshold = 1;
@@ -116,28 +121,26 @@ void database::init_genesis_accounts(const genesis_state_type& genesis_state)
     }
 }
 
-void database::init_genesis_witnesses(const genesis_state_type& genesis_state)
+void db_genesis::init_witnesses()
 {
-    const std::vector<genesis_state_type::witness_type>& witnesses = genesis_state.witness_candidates;
-
-    for (auto& witness : witnesses)
+    for (auto& witness : _genesis_state.witness_candidates)
     {
         FC_ASSERT(!witness.owner_name.empty(), "Witness 'owner_name' should not be empty.");
 
-        create<witness_object>([&](witness_object& w) {
+        _db.create<witness_object>([&](witness_object& w) {
             w.owner = witness.owner_name;
             w.signing_key = witness.block_signing_key;
             w.schedule = witness_object::top20;
-            w.hardfork_time_vote = get_genesis_time();
+            w.hardfork_time_vote = _db.get_genesis_time();
         });
     }
 }
 
-void database::init_genesis_witness_schedule(const genesis_state_type& genesis_state)
+void db_genesis::init_witness_schedule()
 {
-    const std::vector<genesis_state_type::witness_type>& witness_candidates = genesis_state.witness_candidates;
+    const std::vector<genesis_state_type::witness_type>& witness_candidates = _genesis_state.witness_candidates;
 
-    create<witness_schedule_object>([&](witness_schedule_object& wso) {
+    _db.create<witness_schedule_object>([&](witness_schedule_object& wso) {
         for (size_t i = 0; i < wso.current_shuffled_witnesses.size() && i < witness_candidates.size(); ++i)
         {
             wso.current_shuffled_witnesses[i] = witness_candidates[i].owner_name;
@@ -145,13 +148,13 @@ void database::init_genesis_witness_schedule(const genesis_state_type& genesis_s
     });
 }
 
-void database::init_genesis_global_property_object(const genesis_state_type& genesis_state)
+void db_genesis::init_global_property_object()
 {
-    create<dynamic_global_property_object>([&](dynamic_global_property_object& gpo) {
-        gpo.time = get_genesis_time();
+    _db.create<dynamic_global_property_object>([&](dynamic_global_property_object& gpo) {
+        gpo.time = _db.get_genesis_time();
         gpo.recent_slots_filled = fc::uint128::max_value();
         gpo.participation_count = 128;
-        gpo.current_supply = asset(genesis_state.init_supply, SCORUM_SYMBOL);
+        gpo.current_supply = asset(_genesis_state.init_supply, SCORUM_SYMBOL);
         gpo.maximum_block_size = SCORUM_MAX_BLOCK_SIZE;
 
         gpo.total_reward_fund_scorum = asset(0, SCORUM_SYMBOL);
@@ -159,13 +162,13 @@ void database::init_genesis_global_property_object(const genesis_state_type& gen
     });
 }
 
-void database::init_genesis_rewards(const genesis_state_type& genesis_state)
+void db_genesis::init_rewards()
 {
-    const auto& gpo = get_dynamic_global_properties();
+    const auto& gpo = _db.get_dynamic_global_properties();
 
-    auto post_rf = create<reward_fund_object>([&](reward_fund_object& rfo) {
+    auto post_rf = _db.create<reward_fund_object>([&](reward_fund_object& rfo) {
         rfo.name = SCORUM_POST_REWARD_FUND_NAME;
-        rfo.last_update = head_block_time();
+        rfo.last_update = _db.head_block_time();
         rfo.percent_curation_rewards = SCORUM_1_PERCENT * 25;
         rfo.percent_content_rewards = SCORUM_100_PERCENT;
         rfo.reward_balance = gpo.total_reward_fund_scorum;
@@ -178,17 +181,17 @@ void database::init_genesis_rewards(const genesis_state_type& genesis_state)
     FC_ASSERT(post_rf.id._id == 0);
 
     // We share initial fund between raward_pool and fund budget
-    dbs_reward& reward_service = obtain_service<dbs_reward>();
-    dbs_budget& budget_service = obtain_service<dbs_budget>();
+    dbs_reward& reward_service = _db.obtain_service<dbs_reward>();
+    dbs_budget& budget_service = _db.obtain_service<dbs_budget>();
 
-    asset initial_reward_pool_supply(genesis_state.init_rewards_supply.amount
+    asset initial_reward_pool_supply(_genesis_state.init_rewards_supply.amount
                                          * SCORUM_GUARANTED_REWARD_SUPPLY_PERIOD_IN_DAYS
                                          / SCORUM_REWARDS_INITIAL_SUPPLY_PERIOD_IN_DAYS,
-                                     genesis_state.init_rewards_supply.symbol);
-    fc::time_point deadline = get_genesis_time() + fc::days(SCORUM_REWARDS_INITIAL_SUPPLY_PERIOD_IN_DAYS);
+                                     _genesis_state.init_rewards_supply.symbol);
+    fc::time_point deadline = _db.get_genesis_time() + fc::days(SCORUM_REWARDS_INITIAL_SUPPLY_PERIOD_IN_DAYS);
 
     reward_service.create_pool(initial_reward_pool_supply);
-    budget_service.create_fund_budget(genesis_state.init_rewards_supply - initial_reward_pool_supply, deadline);
+    budget_service.create_fund_budget(_genesis_state.init_rewards_supply - initial_reward_pool_supply, deadline);
 }
 
 } // namespace chain
