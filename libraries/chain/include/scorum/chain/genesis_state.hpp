79976ccf45b0cc760a2daac6b1e865823bccabe2
Alexander Plevako <alexplevako@gmail.com>
2017-12-15 09:51:22 +0300

    remove generated genesis

--- libraries/chain/include/scorum/chain/genesis_state.hpp
+++ libraries/chain/include/scorum/chain/genesis_state.hpp
@@ -49,11 +49,6 @@ struct genesis_state_type
     sp::chain_id_type initial_chain_id;
 };
 
-namespace utils {
-
-void generate_default_genesis_state(genesis_state_type& genesis);
-
-} // namespace utils
 } // namespace chain
 } // namespace scorum
 
