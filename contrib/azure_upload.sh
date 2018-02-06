b16ddadb5af23d6c49af10c2cc23ced8bef5e4c7
Alexander Trigubov <alexander.trigubov@scorum.com>
2018-02-06 17:02:28 +0300

    azure_upload.sh - fix tar operation

--- contrib/azure_upload.sh
+++ contrib/azure_upload.sh
@@ -3,7 +3,7 @@
 if [ "$AZURE_UPLOAD" = true ]; then
   ARCHIVE_NAME=scorum_$(echo $GIT_BRANCH | cut -d '/' -f 2)_${GIT_COMMIT:0:7}.tar.gz
   echo "Creating arhive with base files"
-  /bin/tar -cvzf /var/lib/scorumd/$ARCHIVE_NAME /usr/local/scorumd-full/bin/scorumd /usr/local/scorumd-full/bin/cli_wallet
+  /bin/tar czf /var/lib/scorumd/$ARCHIVE_NAME --directory=/usr/local/scorumd-full/bin scorumd cli_wallet
   echo "Upload arhive to azure"
   /usr/local/bin/az storage blob upload --container-name $AZURE_CONTAINER_NAME --file /var/lib/scorumd/$ARCHIVE_NAME --name $ARCHIVE_NAME
 else
