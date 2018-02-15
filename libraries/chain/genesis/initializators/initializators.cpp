c4fa5f0ae05771c815b0cb149c97ec46a35154de
Andrew Masilevich <andrew.masilevich@scorum.com>
2018-02-15 17:22:10 +0300

    Removed formatter for boost type pretty_name.

--- libraries/chain/genesis/initializators/initializators.cpp
+++ libraries/chain/genesis/initializators/initializators.cpp
@@ -20,25 +20,6 @@ initializator& initializator::after(initializator& impl)
     return *this;
 }
 
-namespace {
-std::string get_short_name(const std::string& long_name)
-{
-    static const std::string ns_separator("::");
-    std::string ret(long_name);
-    std::size_t pos = ret.rfind(ns_separator);
-    if (pos != std::string::npos)
-    {
-        ret = ret.substr(pos + ns_separator.length());
-    }
-    pos = ret.rfind('_');
-    if (pos != std::string::npos)
-    {
-        ret = ret.substr(0, pos);
-    }
-    return ret;
-}
-}
-
 void initializator::apply(initializator_context& ctx)
 {
     if (_applied)
@@ -51,7 +32,7 @@ void initializator::apply(initializator_context& ctx)
         r.apply(ctx);
     }
 
-    auto impl = get_short_name(boost::typeindex::type_id_runtime(*this).pretty_name());
+    auto impl = boost::typeindex::type_id_runtime(*this).pretty_name();
 
     dlog("Genesis ${impl} is processing.", ("impl", impl));
 
