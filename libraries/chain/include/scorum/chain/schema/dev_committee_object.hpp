2b5aeccb773e22bb5aedd80184cae06d8b780f32
Alexander Plevako <alexplevako@gmail.com>
2018-03-01 16:22:50 +0300

    added dev_committee_object

--- libraries/chain/include/scorum/chain/schema/dev_committee_object.hpp
+++ libraries/chain/include/scorum/chain/schema/dev_committee_object.hpp
@@ -20,6 +20,17 @@ public:
     asset scr_balance = asset(0, SCORUM_SYMBOL);
 };
 
+class dev_committee_member_object : public object<dev_committee_member_object_type, dev_committee_member_object>
+{
+public:
+    typedef std::reference_wrapper<const dev_committee_member_object> cref_type;
+
+    CHAINBASE_DEFAULT_CONSTRUCTOR(dev_committee_member_object)
+
+    id_type id;
+    account_name_type account;
+};
+
 typedef shared_multi_index_container<dev_committee_object,
                                      indexed_by<ordered_unique<tag<by_id>,
                                                                member<dev_committee_object,
@@ -27,8 +38,23 @@ typedef shared_multi_index_container<dev_committee_object,
                                                                       &dev_committee_object::id>>>>
     dev_committee_index;
 
+struct by_account_name;
+
+typedef shared_multi_index_container<dev_committee_member_object,
+                                     indexed_by<ordered_unique<tag<by_id>,
+                                                               member<dev_committee_member_object,
+                                                                      dev_committee_member_id_type,
+                                                                      &dev_committee_member_object::id>>,
+                                                ordered_unique<tag<by_account_name>,
+                                                               member<dev_committee_member_object,
+                                                                      account_name_type,
+                                                                      &dev_committee_member_object::account>>>>
+    dev_committee_member_index;
+
 } // namespace chain
 } // namespace scorum
 
 FC_REFLECT(scorum::chain::dev_committee_object, (id)(sp_balance)(scr_balance))
 CHAINBASE_SET_INDEX_TYPE(scorum::chain::dev_committee_object, scorum::chain::dev_committee_index)
+
+CHAINBASE_SET_INDEX_TYPE(scorum::chain::dev_committee_member_object, scorum::chain::dev_committee_member_index)
