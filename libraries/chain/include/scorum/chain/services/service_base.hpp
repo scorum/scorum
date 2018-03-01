fecd204d3f8acfe99e87bf498340f4446567ec37
Alexander Abolin <alexander.abolin@scorum.com>
2018-03-01 13:54:14 +0300

    added dbs_service_base class

---
+++ libraries/chain/include/scorum/chain/services/service_base.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <scorum/chain/services/dbs_base.hpp>
+
+namespace scorum {
+namespace chain {
+
+template <class T> struct base_service_i
+{
+    using object_type = T;
+    using modifier_type = std::function<void(object_type&)>;
+
+    virtual ~base_service_i()
+    {
+    }
+
+    virtual const object_type& create(const modifier_type& modifier) = 0;
+
+    virtual void update(const modifier_type& modifier) = 0;
+
+    virtual void remove() = 0;
+
+    virtual bool is_exists() const = 0;
+
+    virtual const object_type& get() const = 0;
+};
+
+template <class service_interface> class dbs_service_base : public dbs_base, public service_interface
+{
+    friend class dbservice_dbs_factory;
+
+protected:
+    explicit dbs_service_base(database& db)
+        : _base_type(db)
+    {
+    }
+
+public:
+    using modifier_type = typename service_interface::modifier_type;
+    using object_type = typename service_interface::object_type;
+
+    // for service operating with single object
+    virtual const object_type& create(const modifier_type& modifier) override
+    {
+        return db_impl().template create<object_type>([&](object_type& o) { modifier(o); });
+    }
+
+    virtual void update(const modifier_type& modifier) override
+    {
+        db_impl().modify(get(), [&](object_type& o) { modifier(o); });
+    }
+
+    virtual void remove() override
+    {
+        db_impl().remove(get());
+    }
+
+    virtual bool is_exists() const override
+    {
+        return nullptr != db_impl().template find<object_type>();
+    }
+
+    virtual const object_type& get() const override
+    {
+        try
+        {
+            return db_impl().template get<object_type>();
+        }
+        FC_CAPTURE_AND_RETHROW()
+    }
+};
+} // namespace chain
+} // namespace scorum
