42805870cd7b73dc396c6bd9ad423a257a7f1882
Alexander Plevako <alexplevako@gmail.com>
2018-03-05 11:27:50 +0300

    added implementation for dev_pool withdraw_vesting evaluator:

--- libraries/chain/evaluators/proposal_evaluators.cpp
+++ libraries/chain/evaluators/proposal_evaluators.cpp
@@ -3,6 +3,8 @@
 #include <scorum/chain/services/account.hpp>
 #include <scorum/chain/services/dev_pool.hpp>
 
+#include <scorum/chain/evaluators/withdraw_vesting_evaluator.hpp>
+
 namespace scorum {
 namespace chain {
 
@@ -35,7 +37,9 @@ development_committee_withdraw_vesting_evaluator::development_committee_withdraw
 void development_committee_withdraw_vesting_evaluator::do_apply(
     const development_committee_withdraw_vesting_evaluator::operation_type& o)
 {
-    FC_ASSERT(false, "development_committee_withdraw_vesting_evaluator not implemented");
+    withdraw_vesting_dev_pool_task create_withdraw;
+    withdraw_vesting_context ctx(db(), o.vesting_shares);
+    create_withdraw.apply(ctx);
 }
 
 } // namespace chain
