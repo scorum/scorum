c8ec143e354415e853a89d3cc159c89814509795
Alexander Abolin <alexander.abolin@scorum.com>
2018-03-29 15:49:08 +0300

    Fix fund budget distribution taking under consideration missing blocks

--- tests/chain_tests/budget/fund_budget_tests.cpp
+++ tests/chain_tests/budget/fund_budget_tests.cpp
@@ -63,6 +63,33 @@ SCORUM_TEST_CASE(try_close_fund_budget)
     BOOST_REQUIRE_THROW(budget_service.close_budget(budget), fc::assert_exception);
 }
 
+SCORUM_TEST_CASE(fund_budget_payments_after_deadline_is_over_and_we_have_missing_blocks)
+{
+    auto& budget = budget_service.get_fund_budget();
+
+    auto original_balance = budget.balance;
+
+    auto miss_intermediate_blocks = true;
+
+    auto actually_generated_blocks
+        = db_plugin->debug_generate_blocks_until(debug_key, budget.deadline, miss_intermediate_blocks, default_skip);
+    BOOST_REQUIRE((db.head_block_time() - budget.deadline).to_seconds() < SCORUM_BLOCK_INTERVAL);
+
+    BOOST_REQUIRE(budget_service.is_fund_budget_exists());
+
+    BOOST_REQUIRE_EQUAL(budget.balance,
+                        original_balance - asset(budget.per_block, SCORUM_SYMBOL) * actually_generated_blocks);
+
+    // generate block after deadline
+    generate_block();
+    ++actually_generated_blocks;
+
+    BOOST_REQUIRE(budget_service.is_fund_budget_exists());
+
+    BOOST_REQUIRE_EQUAL(budget.balance,
+                        original_balance - asset(budget.per_block, SCORUM_SYMBOL) * actually_generated_blocks);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 #endif
