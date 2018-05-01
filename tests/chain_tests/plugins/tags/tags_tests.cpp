035e5b4be8f60862d623a8dd8569ac511d7f2340
Alexander Plevako <alexplevako@gmail.com>
2018-05-01 14:44:04 +0300

    added tests for get_discussions_by_created

--- tests/chain_tests/plugins/tags/tags_tests.cpp
+++ tests/chain_tests/plugins/tags/tags_tests.cpp
@@ -108,7 +108,7 @@ struct tags_fixture : public database_fixture::database_trx_integration_fixture
 
         push_operation<comment_operation>(operation, actor.private_key);
 
-        generate_blocks(20 / SCORUM_BLOCK_INTERVAL);
+        generate_blocks(5 * 60 / SCORUM_BLOCK_INTERVAL);
 
         return Comment(operation, *this);
     }
@@ -132,22 +132,20 @@ BOOST_FIXTURE_TEST_SUITE(tags_tests, tags_fixture)
 
 SCORUM_TEST_CASE(get_discussions_by_created)
 {
-    {
-        api::discussion_query query;
-        query.limit = 1;
+    api::discussion_query query;
+    query.limit = 1;
 
-        BOOST_REQUIRE_EQUAL(_api.get_discussions_by_created(query).size(), 0u);
+    BOOST_REQUIRE_EQUAL(_api.get_discussions_by_created(query).size(), 0u);
 
-        create_post(initdelegate, [](comment_operation& op) {
-            op.title = "root post";
-            op.body = "body";
-        });
+    create_post(initdelegate, [](comment_operation& op) {
+        op.title = "root post";
+        op.body = "body";
+    });
 
-        BOOST_REQUIRE_EQUAL(_api.get_discussions_by_created(query).size(), 1u);
-    }
+    BOOST_REQUIRE_EQUAL(_api.get_discussions_by_created(query).size(), 1u);
 }
 
-SCORUM_TEST_CASE(test_depth)
+SCORUM_TEST_CASE(test_comments_depth_counter)
 {
     auto get_comments_with_depth = [](scorum::chain::database& db) {
         std::map<std::string, uint16_t> result;
@@ -182,6 +180,57 @@ SCORUM_TEST_CASE(test_depth)
     BOOST_REQUIRE_EQUAL(0u, check_list[root.permlink()]);
     BOOST_REQUIRE_EQUAL(1u, check_list[root_child.permlink()]);
     BOOST_REQUIRE_EQUAL(2u, check_list[root_child_child.permlink()]);
+}
+
+SCORUM_TEST_CASE(get_discussions_by_created_return_post_and_it_comments_if_its_id_0)
+{
+    auto root = create_post(initdelegate, [](comment_operation& op) {
+        op.title = "root";
+        op.body = "body";
+    });
+
+    auto comment_level_1 = root.create_comment(initdelegate, [](comment_operation& op) {
+        op.title = "level 1";
+        op.body = "body";
+    });
+
+    api::discussion_query query;
+    query.limit = 100;
+
+    BOOST_REQUIRE_EQUAL(_api.get_discussions_by_created(query).size(), 2u);
+}
+
+SCORUM_TEST_CASE(get_discussions_by_created_return_two_posts)
+{
+    create_post(initdelegate, [](comment_operation& op) {
+        op.title = "zero";
+        op.body = "post";
+    });
+
+    auto root = create_post(initdelegate, [](comment_operation& op) {
+        op.title = "one";
+        op.body = "post";
+    });
+
+    auto comment_level_1 = root.create_comment(initdelegate, [](comment_operation& op) {
+        op.title = "level 1";
+        op.body = "body";
+    });
+
+    auto comment_level_2 = comment_level_1.create_comment(initdelegate, [](comment_operation& op) {
+        op.title = "level 2";
+        op.body = "body";
+    });
+
+    api::discussion_query query;
+    query.limit = 100;
+
+    const auto discussions = _api.get_discussions_by_created(query);
+
+    BOOST_CHECK_EQUAL(discussions.size(), 2u);
+
+    BOOST_CHECK_EQUAL(discussions[0].permlink, "one");
+    BOOST_CHECK_EQUAL(discussions[1].permlink, "zero");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
