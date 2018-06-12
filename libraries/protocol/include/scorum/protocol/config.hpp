507f5b144f63a234d4d5d6229fa5134d69360096
Andrew Masilevich <andrew.masilevich@scorum.com>
2018-06-12 12:49:44 +0300

    Fixed version

--- libraries/protocol/include/scorum/protocol/config.hpp
+++ libraries/protocol/include/scorum/protocol/config.hpp
@@ -74,7 +74,7 @@ namespace detail {
 
 #define DAYS_TO_SECONDS(X)                     (60u*60u*24u*X)
 
-#define SCORUM_BLOCKCHAIN_VERSION              ( version(0, 0, 2) )
+#define SCORUM_BLOCKCHAIN_VERSION              ( version(0, 0, 3) )
 #define SCORUM_BLOCKCHAIN_HARDFORK_VERSION     ( hardfork_version( SCORUM_BLOCKCHAIN_VERSION ) )
 
 #define SCORUM_ADDRESS_PREFIX                  "SCR"
