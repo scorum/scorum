fd6fa3aba8a420306af8a43b1e192fd3b894f34d
Andrew Masilevich <andrew.masilevich@scorum.com>
2018-06-12 12:13:04 +0300

    Fast fix for fork bug

--- libraries/chain/include/scorum/chain/database/block_tasks/block_tasks.hpp
+++ libraries/chain/include/scorum/chain/database/block_tasks/block_tasks.hpp
@@ -36,6 +36,7 @@ private:
     uint32_t _block_num;
 };
 
+// TODO: don`t use this guard for apply_block!
 template <uint32_t per_block_num> class per_block_num_apply_guard : public task_reentrance_guard_i<block_task_context>
 {
     static_assert(per_block_num > 0u, "Invalid value for per_block_num.");
@@ -55,8 +56,7 @@ private:
     uint32_t _last_block_num = 0u;
 };
 
-template <uint32_t per_block_num>
-class block_task_type : public task<block_task_context, per_block_num_apply_guard<per_block_num>>
+class block_task_type : public task<block_task_context>
 {
 protected:
     block_task_type()
@@ -66,7 +66,7 @@ protected:
 
 // By default guard (per_block_num_apply_guard) controls applying one time per one block.
 // Use class inherited from block_task in this case.
-using block_task = block_task_type<1u>;
+using block_task = block_task_type;
 // If it is required applying one time per N block, use class inherited from block_task_type<N>
 
 } // database_ns
