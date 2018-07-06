a411b996a0249ebf9eb0c37fe7b9e5ebe810fa4f
Katkevich Dmitry <dmitry.katkevich@scorum.com>
2018-07-06 11:00:31 +0300

    Updated version to 0.0.5

--- libraries/protocol/include/scorum/protocol/config.hpp
+++ libraries/protocol/include/scorum/protocol/config.hpp
@@ -74,7 +74,7 @@ namespace detail {
 
 #define DAYS_TO_SECONDS(X)                     (60u*60u*24u*X)
 
-#define SCORUM_BLOCKCHAIN_VERSION              ( version(0, 0, 4) )
+#define SCORUM_BLOCKCHAIN_VERSION              ( version(0, 0, 5) )
 #define SCORUM_BLOCKCHAIN_HARDFORK_VERSION     ( hardfork_version( SCORUM_BLOCKCHAIN_VERSION ) )
 
 #define SCORUM_ADDRESS_PREFIX                  "SCR"
