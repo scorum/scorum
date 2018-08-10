981729c0f6c8df96b4a52452101e3042a74e1c97
Katkevich Dmitry <dmitry.katkevich@scorum.com>
2018-08-10 15:39:16 +0300

    Fixed betting_service after merge

--- libraries/chain/services/betting_service.cpp
+++ libraries/chain/services/betting_service.cpp
@@ -22,5 +22,30 @@ bool dbs_betting::is_betting_moderator(const account_name_type& account_name) co
     }
     FC_CAPTURE_LOG_AND_RETHROW((account_name))
 }
+
+void dbs_betting::return_unresolved_bets(const game_object& game)
+{
+    boost::ignore_unused_variable_warning(game);
+    FC_THROW("not implemented");
+}
+
+void dbs_betting::return_bets(const game_object& game, const std::vector<betting::wincase_pair>& cancelled_wincases)
+{
+    boost::ignore_unused_variable_warning(game);
+    boost::ignore_unused_variable_warning(cancelled_wincases);
+    FC_THROW("not implemented");
+}
+
+void dbs_betting::remove_disputs(const game_object& game)
+{
+    boost::ignore_unused_variable_warning(game);
+    FC_THROW("not implemented");
+}
+
+void dbs_betting::remove_bets(const game_object& game)
+{
+    boost::ignore_unused_variable_warning(game);
+    FC_THROW("not implemented");
+}
 }
 }
