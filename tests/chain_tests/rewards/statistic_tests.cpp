9e05838751bb6d0059b690327a191d874beaceb2
Andrew Masilevich <andrew.masilevich@scorum.com>
2018-08-27 11:27:03 +0300

    Add new tests

--- tests/chain_tests/rewards/statistic_tests.cpp
+++ tests/chain_tests/rewards/statistic_tests.cpp
@@ -7,6 +7,10 @@
 #include <scorum/chain/services/comment.hpp>
 #include <scorum/chain/services/comment_statistic.hpp>
 
+#include <scorum/tags/tags_objects.hpp>
+#include <scorum/tags/tags_api.hpp>
+#include <scorum/tags/tags_plugin.hpp>
+
 #include <boost/fusion/include/map.hpp>
 #include <boost/fusion/include/at_key.hpp>
 
@@ -14,6 +18,8 @@ namespace rewards_blogging_statistic_tests {
 
 using namespace scorum::chain;
 using namespace scorum::protocol;
+using namespace scorum::app;
+using namespace scorum::tags;
 
 using namespace database_fixture;
 
@@ -27,7 +33,11 @@ struct rewards_blogging_statistic_fixture : public database_blog_integration_fix
         , andrew("andrew")
         , comments(db.comment_service())
         , statistic(db.comment_statistic_sp_service())
+        , _api_ctx(app, TAGS_API_NAME, std::make_shared<api_session_data>())
+        , tag_api(_api_ctx)
     {
+        init_plugin<scorum::tags::tags_plugin>();
+
         open_database();
 
         create_actor(alice);
@@ -57,7 +67,12 @@ struct rewards_blogging_statistic_fixture : public database_blog_integration_fix
     //  'Andrew' vote for 'Sam' comment
     test_case_1 create_test_case_1()
     {
-        auto p = create_post(alice).set_beneficiar(bob, 30).in_block();
+        auto p
+            = create_post(alice)
+                  .set_beneficiar(bob, 30)
+                  .set_json(
+                      R"({"domains": ["chain_tests"], "categories": ["rewards_blogging_statistic_tests"], "tags": ["test"]})")
+                  .in_block();
 
         auto c = p.create_comment(sam).in_block();
 
@@ -80,6 +95,9 @@ struct rewards_blogging_statistic_fixture : public database_blog_integration_fix
 
     comment_service_i& comments;
     comment_statistic_sp_service_i& statistic;
+
+    api_context _api_ctx;
+    scorum::tags::tags_api tag_api;
 };
 
 BOOST_FIXTURE_TEST_SUITE(rewards_blogging_statistic_tests, rewards_blogging_statistic_fixture)
@@ -167,7 +185,7 @@ SCORUM_TEST_CASE(virtual_operation_statistic_check)
             {
                 auto &stat = bf::at_key<comment_reward_operation_stats>(stats)[op.author];
                 stat.fund_reward += op.fund_reward;
-                stat.total_payout += op.beneficiaries_payout;
+                stat.total_payout += op.total_payout;
                 stat.author_payout += op.author_payout;
                 stat.curators_payout += op.curators_payout;
                 stat.from_children_payout += op.from_children_payout;
@@ -199,7 +217,7 @@ SCORUM_TEST_CASE(virtual_operation_statistic_check)
     const auto& sam_stat
         = statistic.get(comments.get(test_case.sam_comment.author(), test_case.sam_comment.permlink()).id);
 
-    SCORUM_MESSAGE("-- Check comment_reward_operation_stats");
+    SCORUM_MESSAGE("-- Check comment_reward_operation");
     {
         const auto& alice_v_stat = bf::at_key<comment_reward_operation_stats>(stats)[alice];
 
@@ -210,10 +228,110 @@ SCORUM_TEST_CASE(virtual_operation_statistic_check)
         wdump((sam_v_stat));
 
         BOOST_CHECK_EQUAL(alice_v_stat.total_payout, alice_stat.total_payout_value);
+        BOOST_CHECK_EQUAL(alice_v_stat.author_payout, alice_stat.author_payout_value);
+        BOOST_CHECK_EQUAL(alice_v_stat.curators_payout, alice_stat.curator_payout_value);
+        BOOST_CHECK_EQUAL(alice_v_stat.beneficiaries_payout, alice_stat.beneficiary_payout_value);
+        BOOST_CHECK_EQUAL(alice_v_stat.fund_reward, alice_stat.fund_reward_value);
+        BOOST_CHECK_EQUAL(alice_v_stat.from_children_payout, alice_stat.from_children_payout_value);
+        BOOST_CHECK_EQUAL(alice_v_stat.to_parent_payout, alice_stat.to_parent_payout_value);
+
         BOOST_CHECK_EQUAL(sam_v_stat.total_payout, sam_stat.total_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.author_payout, sam_stat.author_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.curators_payout, sam_stat.curator_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.beneficiaries_payout, sam_stat.beneficiary_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.fund_reward, sam_stat.fund_reward_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.from_children_payout, sam_stat.from_children_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.to_parent_payout, sam_stat.to_parent_payout_value);
     }
 
-    // TODO
+    SCORUM_MESSAGE("-- Check author_reward_operation");
+    {
+        const auto& alice_v_stat = bf::at_key<author_reward_operation_stats>(stats)[alice];
+
+        wdump((alice_v_stat));
+
+        const auto& sam_v_stat = bf::at_key<author_reward_operation_stats>(stats)[sam];
+
+        wdump((sam_v_stat));
+
+        BOOST_CHECK_EQUAL(alice_v_stat.reward, alice_stat.author_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.reward, sam_stat.author_payout_value);
+    }
+
+    SCORUM_MESSAGE("-- Check curation_reward_operation");
+    {
+        const auto& alice_v_stat = bf::at_key<curation_reward_operation_stats>(stats)[alice];
+
+        wdump((alice_v_stat));
+
+        const auto& sam_v_stat = bf::at_key<curation_reward_operation_stats>(stats)[sam];
+
+        wdump((sam_v_stat));
+
+        BOOST_CHECK_EQUAL(alice_v_stat.reward, alice_stat.curator_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.reward, sam_stat.curator_payout_value);
+    }
+
+    SCORUM_MESSAGE("-- Check comment_benefactor_reward_operation");
+    {
+        const auto& alice_v_stat = bf::at_key<comment_benefactor_reward_operation_stats>(stats)[alice];
+
+        wdump((alice_v_stat));
+
+        const auto& sam_v_stat = bf::at_key<comment_benefactor_reward_operation_stats>(stats)[sam];
+
+        wdump((sam_v_stat));
+
+        BOOST_CHECK_EQUAL(alice_v_stat.reward, alice_stat.beneficiary_payout_value);
+        BOOST_CHECK_EQUAL(sam_v_stat.reward, sam_stat.beneficiary_payout_value);
+    }
+}
+
+SCORUM_TEST_CASE(tag_discussion_statistic_check)
+{
+    auto test_case = create_test_case_1();
+
+    const auto& alice_stat
+        = statistic.get(comments.get(test_case.alice_post.author(), test_case.alice_post.permlink()).id);
+    const auto& sam_stat
+        = statistic.get(comments.get(test_case.sam_comment.author(), test_case.sam_comment.permlink()).id);
+
+    auto alice_discussion = tag_api.get_content(test_case.alice_post.author(), test_case.alice_post.permlink());
+    auto comments = tag_api.get_comments(test_case.alice_post.author(), test_case.alice_post.permlink());
+    BOOST_REQUIRE(!comments.empty());
+    auto sam_discussion = comments[0];
+
+    wdump((alice_discussion));
+
+    BOOST_CHECK_EQUAL(alice_discussion.total_payout_sp_value, alice_stat.total_payout_value);
+    BOOST_CHECK_EQUAL(alice_discussion.author_payout_sp_value, alice_stat.author_payout_value);
+    BOOST_CHECK_EQUAL(alice_discussion.curator_payout_sp_value, alice_stat.curator_payout_value);
+    BOOST_CHECK_EQUAL(alice_discussion.beneficiary_payout_sp_value, alice_stat.beneficiary_payout_value);
+    BOOST_CHECK_EQUAL(alice_discussion.from_children_payout_sp_value, alice_stat.from_children_payout_value);
+    BOOST_CHECK_EQUAL(alice_discussion.to_parent_payout_sp_value, alice_stat.to_parent_payout_value);
+
+    wdump((sam_discussion));
+
+    BOOST_CHECK_EQUAL(sam_discussion.total_payout_sp_value, sam_stat.total_payout_value);
+    BOOST_CHECK_EQUAL(sam_discussion.author_payout_sp_value, sam_stat.author_payout_value);
+    BOOST_CHECK_EQUAL(sam_discussion.curator_payout_sp_value, sam_stat.curator_payout_value);
+    BOOST_CHECK_EQUAL(sam_discussion.beneficiary_payout_sp_value, sam_stat.beneficiary_payout_value);
+    BOOST_CHECK_EQUAL(sam_discussion.from_children_payout_sp_value, sam_stat.from_children_payout_value);
+    BOOST_CHECK_EQUAL(sam_discussion.to_parent_payout_sp_value, sam_stat.to_parent_payout_value);
+}
+
+SCORUM_TEST_CASE(tag_total_statistic_check)
+{
+    auto test_case = create_test_case_1();
+
+    const auto& alice_stat
+        = statistic.get(comments.get(test_case.alice_post.author(), test_case.alice_post.permlink()).id);
+
+    auto posts = tag_api.get_trending_tags("test", 2);
+    BOOST_REQUIRE(!posts.empty());
+    auto alice_tag_stat = posts[0];
+
+    BOOST_CHECK_EQUAL(alice_tag_stat.total_payouts_sp, alice_stat.total_payout_value);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
