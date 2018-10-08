4414b283ef6f52b6e58e696f2863a11a7789c5d0
Alexander Plevako <alexplevako@gmail.com>
2018-10-08 16:02:31 +0300

    ttt

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-git submodule update --init --recursive
+exit 1
 
-ciscripts/triggerbuild.sh
+# git submodule update --init --recursive
 
-if [ $? -eq 0  ]; then
-	ciscripts/buildsuccess.sh
-	exit 0
-else
-	ciscripts/buildfailure.sh
-	exit 1
-fi
+# ciscripts/triggerbuild.sh
+
+# if [ $? -eq 0  ]; then
+# 	ciscripts/buildsuccess.sh
+# 	exit 0
+# else
+# 	ciscripts/buildfailure.sh
+# 	exit 1
+# fi
 
