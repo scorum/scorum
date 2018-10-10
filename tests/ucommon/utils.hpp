ca3f80cad43a7371af78266f619c08c3a3167030
Katkevich Dmitry <dmitry.katkevich@scorum.com>
2018-10-10 12:36:06 +0300

    BLOC-555 -- Changed UUID json serialization from binary blob to string + added serialization tests

--- tests/ucommon/utils.hpp
+++ tests/ucommon/utils.hpp
@@ -17,4 +17,13 @@ template <typename T> std::string to_hex(const T& obj)
     return fc::to_hex(buf.c_str(), buf.size());
 }
 
+template <typename T> T from_hex(const std::string& bin_hex)
+{
+    char buffer[sizeof(T)];
+    fc::from_hex(bin_hex, buffer, sizeof(buffer));
+    auto obj = fc::raw::unpack<T>(buffer, sizeof(buffer));
+
+    return obj;
+}
+
 } // namespace utils
